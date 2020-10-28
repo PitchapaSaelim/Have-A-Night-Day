47027ba23115982466d633888b49c54c8579f18e
PanidaOun <panida.ou@ku.th>
2020-10-28 22:47:30 +0700

    Add Test

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.test.client import Client
+from django.urls import reverse
+from .models import Author
 
-# Create your tests here.
+
+class AuthorModelTest(TestCase):
+    
+    def test_user_name_in_database(self):
+        User.objects.create_user(username='Big',password='@Parn123')
+        user1 = User.objects.filter(username='Big').first()
+        self.assertEqual(user1.username,'Big')
+
+    def test_first_name_max_length(self):
+        User.objects.create(first_name='Big', last_name='Bob')
+        author = User.objects.get(id=1)
+        max_length = author._meta.get_field('first_name').max_length
+        self.assertEqual(max_length, 150)
+
+    def test_addAccount(self):
+        response = self.client.post(reverse("register_user"),{'username':'Parn', 'fname':'Panida', 'lname':'Ounnaitham', 'password1':'Saiparn021143', 'password2':'Saiparn021143'})
+        self.assertEqual(response.status_code, 302)
+
+    
