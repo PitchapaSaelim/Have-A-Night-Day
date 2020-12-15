f84423b685108596c0d4627bdc0e6fe50b173364
PitchapaSaelim <pitchapa.sae@ku.th>
2020-12-16 02:16:00 +0700

    Add the average of sleep data and fixed gender field

--- user/models.py
+++ user/models.py
@@ -3,11 +3,26 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from sleep_time_management.models import Eventtime
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birth_date = models.DateField(null=True, blank=True)
-    gender = models.CharField(max_length=100, default='')
+    gender = models.CharField(max_length=6, default='')
+    age_span = models.CharField(max_length=100, default='')
+    average = models.CharField(max_length=100, default='', help_text='average sleep time')
+
+    def calculate_average(self):
+        queryset = Eventtime.objects.filter(
+            user=self.user).values('sleep_data')
+        count = 0
+        sums = 0
+        for i in queryset:
+            time = i['sleep_data'].split(" ")
+            count += 1
+            sums += float(time[0])
+        return sums/count
 
     def __str__(self):
         return self.user.username
