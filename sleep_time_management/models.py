35d28762fa7ec6065b8e4b0d269c61b3969a342a
PitchapaSaelim <pitchapa.sae@ku.th>
2020-12-16 01:57:25 +0700

    Updated Bar Chart And Collect Sleep Data Each Day

--- sleep_time_management/models.py
+++ sleep_time_management/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -6,41 +5,28 @@ from django.dispatch import receiver
 
 
 class Eventtime(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     wake_time = models.IntegerField(default=0, help_text='wake time')
     bed_time = models.IntegerField(default=0, help_text='bedtime')
     bed_event_time = models.IntegerField(default=0, help_text='bed event time')
     wake_event_time = models.IntegerField(
         default=0, help_text='wake event time')
     sleep_data = models.TextField(default='', help_text='sleep data')
-    # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='timeevents')
+    date = models.DateTimeField('date eventtime', null=True)
 
     def __str__(self):
         return self.user.username
 
     def calculate_sleep_wake_data(self):
-        # if self.wake_time < self.bed_event_time:
-        #     self.wake_time += 24*60
-        # get_wake_data = self.wake_time - self.bed_event_time
-        # get_hr_wake_data = get_wake_data//60
-        # get_min_wake_data = get_wake_data % 60
-        # sleep_data = f"{get_hr_wake_data}.{get_min_wake_data} hours"
-        sleep_data = calculate_sleep_wake(self.wake_time,self.bed_event_time)
+        sleep_data = calculate_sleep_wake(self.wake_time, self.bed_event_time)
         return sleep_data
-
-
 
     def calculate_sleep_bed_data(self):
-        # if self.wake_event_time < self.bed_time:
-        #     self.wake_event_time += 24*60
-        # get_bed_data = self.wake_event_time - self.bed_time
-        # get_hr_bed_data = get_bed_data//60
-        # get_min_bed_data = get_bed_data % 60
-        # sleep_data = f"{get_hr_bed_data}.{get_min_bed_data} hours"
-        sleep_data = calculate_sleep_bed(self.bed_time,self.wake_event_time)
+        sleep_data = calculate_sleep_bed(self.bed_time, self.wake_event_time)
         return sleep_data
 
-def calculate_sleep_wake(waketime,bed_event_time):
+
+def calculate_sleep_wake(waketime, bed_event_time):
     if waketime < bed_event_time:
         waketime += 24*60
     get_wake_data = waketime - bed_event_time
@@ -49,7 +35,8 @@ def calculate_sleep_wake(waketime,bed_event_time):
     sleep_data = f"{get_hr_wake_data}.{get_min_wake_data} hours"
     return sleep_data
 
-def calculate_sleep_bed(bedtime,wake_event_time):
+
+def calculate_sleep_bed(bedtime, wake_event_time):
     if wake_event_time < bedtime:
         wake_event_time += 24*60
     get_bed_data = wake_event_time - bedtime
@@ -57,9 +44,4 @@ def calculate_sleep_bed(bedtime,wake_event_time):
     get_min_bed_data = get_bed_data % 60
     sleep_data = f"{get_hr_bed_data}.{get_min_bed_data} hours"
     return sleep_data
-    
-@receiver(post_save, sender=User)
-def update_user_eventtime(sender, instance, created, **kwargs):
-    if created:
-        Eventtime.objects.create(user=instance)
-    instance.eventtime.save()
+
--- sleep_time_management/views.py
+++ sleep_time_management/views.py
@@ -9,20 +9,125 @@ from .models import Eventtime
 
 from django.http import JsonResponse
 
+from django.utils import timezone
+from datetime import date
+from django.contrib import messages
+
+
+def get_age_span(age):
+    if 1 >= age or age <= 2:
+        return 'Toddler'
+    elif 3 >= age or age <= 5:
+        return 'Preschool'
+    elif 6 >= age or age <= 13:
+        return 'School age'
+    elif 14 >= age or age <= 17:
+        return 'Teenager'
+    elif 18 >= age or age <= 25:
+        return 'Young adult'
+    elif 26 >= age or age <= 64:
+        return 'Adult'
+    elif age >= 65:
+        return 'Older adult'
+
+
+def get_sleep_hour(age_span):
+    if age_span == 'Toddler':
+        return '11-14 hours'
+    elif age_span == 'Preschool':
+        return '10-13 hours'
+    elif age_span == 'School age':
+        return '9-11 hours'
+    elif age_span == 'Teenager':
+        return '8-10 hours'
+    elif age_span == 'Young adult':
+        return '7-9 hours'
+    elif age_span == 'Adult':
+        return '7-9 hours'
+    elif age_span == 'Older adult':
+        return '7-8 hours'
+
+
+def get_disease_list(age, gender, average_sleep):
+    disease = []
+    hour = float(average_sleep)
+    if 25 >= age or age <= 59:
+        if hour <= 5:
+            disease.append("Hypertension")
+    elif 60 >= age or age <= 86:
+        if hour >= 9:
+            disease.append("Hypertension")
+    if hour > 10:
+        disease.append("Breast cancer risk")
+    if gender == "Female":
+        if age < 60:
+            if hour < 6:
+                disease.append("Coronary heart disease (CHD)")
+        if age > 65:
+            if hour < 5 or hour > 9:
+                disease.append("Depression")
+        if hour > 9:
+            disease.append("Infertility")
+    elif gender == "Male":
+        if 40 >= age or age <= 79:
+            if hour < 5:
+                disease.append("Coronary heart disease (CHD)")
+        if age > 50:
+            disease.append("REM Sleep Disorder")
+        if 40 >= age or age <= 70:
+            if hour != 7:
+                disease.append(
+                    "Diabetes mellitus (DM) /Impaired glucose tolerance (IGT)")
+        elif 53 >= age or age <= 93:
+            if hour != 7 or hour != 8:
+                disease.append(
+                    "Diabetes mellitus (DM) /Impaired glucose tolerance (IGT)")
+    if 18 >= age or age <= 35:
+        if hour < 6:
+            disease.append("Stroke")
+    elif 35 >= age or age <= 45:
+        if hour < 6:
+            disease.append("Stroke")
+    elif 46 >= age or age <= 55:
+        if hour < 6:
+            disease.append("Stroke")
+    elif 55 >= age or age <= 65:
+        if hour < 6:
+            disease.append("Stroke")
+    elif 65 >= age or age <= 80:
+        if hour > 8:
+            disease.append("Stroke")
+    if hour < 6:
+        disease.append("Colorectal cancer")
+        disease.append("Colon cancer")
+    if hour > 8:
+        disease.append("Hypersomnia")
+    return disease
+
 
 @login_required
 def home(request):
     return render(request, 'sleep_time_management/home.html')
 
 
-@login_required
 def information(request):
     return render(request, 'sleep_time_management/information.html')
 
 
 @login_required
 def mainprofile(request):
-    return render(request, 'sleep_time_management/mainprofile.html')
+    disease_list = []
+    today = date.today()
+    birthday = request.user.profile.birth_date
+    age = today.year - birthday.year - \
+        ((today.month, today.day) < (birthday.month, birthday.day))
+    age_span = get_age_span(age)
+    request.user.profile.age_span = age_span
+    average = request.user.profile.calculate_average()
+    request.user.profile.average = average
+    request.user.profile.save()
+    disease_list = get_disease_list(age, request.user.profile.gender, average)
+    return render(request, 'sleep_time_management/mainprofile.html', {'age': age, 'disease_list': disease_list})
 
 
 @login_required
@@ -78,14 +183,25 @@ def logout_view(request):
 
 def calculate2_view(request):
     if request.method == 'POST':
+        sleep_hour = get_sleep_hour(request.user.profile.age_span)
         waketime = request.POST["waketime"]
-        time = waketime.split(':')
-        caltime = (int(time[0])*60)+int(time[1])
-        wake_time = Eventtime.objects.filter(user=request.user).first()
-        wake_time.wake_time = caltime
-        wake_time.save()
-        listtime = calculate_waketime(waketime=waketime)
-        return render(request, 'sleep_time_management/calculator2.html', {'waketime': listtime})
+        if waketime != "":
+            time = waketime.split(':')
+            caltime = (int(time[0])*60)+int(time[1])
+            today = date.today()
+            if Eventtime.objects.filter(user=request.user, date__day=today.day).exists():
+                wake_time = Eventtime.objects.filter(
+                    user=request.user, date__day=today.day).first()
+            else:
+                wake_time = Eventtime.objects.create(
+                    user=request.user, date=timezone.now())
+            wake_time.wake_time = caltime
+            wake_time.save()
+            listtime = calculate_waketime(waketime=waketime)
+            return render(request, 'sleep_time_management/calculator2.html', {'waketime': listtime, 'sleep_hour': sleep_hour})
+        else:
+            messages.warning(request, "Please input AM or PM")
+            return render(request, 'sleep_time_management/home.html')
 
 
 def calculate_waketime(waketime):
@@ -131,14 +247,21 @@ def calculate_sleeptime(sleeptime):
 
 def calculate3_view(request):
     if request.method == 'POST':
+        sleep_hour = get_sleep_hour(request.user.profile.age_span)
         sleeptime = request.POST["sleeptime"]
-        time = sleeptime.split(':')
-        caltime = (int(time[0])*60)+int(time[1])
-        bed_time = Eventtime.objects.filter(user=request.user).first()
-        bed_time.bed_time = caltime
-        bed_time.save()
-        listtime = calculate_sleeptime(sleeptime=sleeptime)
-        return render(request, 'sleep_time_management/calculator3.html', {'sleeptime': listtime})
+        if sleeptime != "":
+            time = sleeptime.split(':')
+            caltime = (int(time[0])*60)+int(time[1])
+            today = date.today()
+            bed_time = Eventtime.objects.filter(
+                user=request.user, date__day=today.day).first()
+            bed_time.bed_time = caltime
+            bed_time.save()
+            listtime = calculate_sleeptime(sleeptime=sleeptime)
+            return render(request, 'sleep_time_management/calculator3.html', {'sleeptime': listtime, 'sleep_hour': sleep_hour})
+        else:
+            messages.warning(request, "Please input AM or PM")
+            return render(request, 'sleep_time_management/home.html')
 
 
 def bed_sleep_data(request):
@@ -146,7 +269,9 @@ def bed_sleep_data(request):
         get_bed_event_time = request.POST["bed_event_time"]
         time = get_bed_event_time.split(':')
         cal_bed_time = (int(time[0])*60)+int(time[1])
-        bed_event_time = Eventtime.objects.filter(user=request.user).first()
+        today = date.today()
+        bed_event_time = Eventtime.objects.filter(
+            user=request.user, date__day=today.day).first()
         bed_event_time.bed_event_time = cal_bed_time
         bed_event_time.save()
     bed_event_time.sleep_data = bed_event_time.calculate_sleep_wake_data()
@@ -168,14 +293,18 @@ def wake_sleep_data(request):
 
 
 def sleep_chart(request):
-    labels = ['day 1']
+    labels = []
     data = []
+    count = 1
 
     queryset = Eventtime.objects.filter(user=request.user).values('sleep_data')
-    # data.append(queryset['sleep_data'])
+
     for i in queryset:
         time = i['sleep_data'].split(" ")
+        date = "Day " + str(count)
+        labels.append(date)
         data.append(float(time[0]))
+        count += 1
 
     return JsonResponse(data={
         'labels': labels,
